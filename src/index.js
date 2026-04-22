a330cb4f5d32636c0d3ab1911801e1a1c0113f8c
Matheus Cortinovis <cortinovismatheus01@gmail.com>
2026-04-22 14:33:28 -0300

    criando mais coisas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
+const numbers = []
+const select = document.getElementById("numbers")
+
 function check () {
-    
-    let number = document.getElementById("number").value
-
+    var number = document.getElementById("number").value
     console.log(number)
-
+    
     if(number < 1 || number > 100 || number.trim() == ''){
         alert("digite um número válido")
-    } return
+        return
+    }
+
+    numbers.push(number)
+
+    console.log(`Adicionou no array o ${number}`)
+
+    const option = document.createElement("option")
+
+    option.text = `Valor ${number} adicionado`
+    option.value = number
+
+    select.appendChild(option)
 }
 
 
-function main() {
-    check()
+function clear() {
+
 }
